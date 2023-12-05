f92823fba6576fb2ec21274d012d8e71a7f14737
nokas90 <nokas90@gmail.com>
2023-12-05 14:47:35 +0200

    add context to all children inside Providers

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { UsersProvider } from "./contexts/UserContext";
+import ForumQuestionsContext, {
+  ForumQuestionsProvider,
+} from "./contexts/ForumQuestionsContext";
+import { ForumAnswersProvider } from "./contexts/ForumAnswersContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ForumQuestionsProvider>
+      <UsersProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UsersProvider>
+  </ForumQuestionsProvider>
 );
