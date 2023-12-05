99d8f08143b00618e2ff9c1b47c74b79593af8f5
nokas90 <nokas90@gmail.com>
2023-12-05 14:41:34 +0200

    add routes and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import "./App.css";
+import NavBar from "./components/UI/NavBar";
+import Login from "./components/pages/Login";
+import { Routes, Route } from "react-router-dom";
+import Register from "./components/pages/Register";
+import Main from "./components/pages/Main";
+import OneQuestion from "./components/pages/OneQuestion";
+import AllQuestions from "./components/pages/AllQuestions.";
+import AddQuestion from "./components/pages/AddQuestion";
+import AddAnswer from "./components/pages/AddAnswer";
 
 function App() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
+      <Routes>
+        <Route index element={<Main />} />
+        <Route path="/questions">
+          <Route path="allQuestions" element={<AllQuestions />} />
+          <Route path=":id">
+            <Route index element={<OneQuestion />} />
+            <Route path="addAnswer" element={<AddAnswer />} />
+          </Route>
+          <Route path="newQuestion" element={<AddQuestion />} />
+        </Route>
+        <Route path="/user">
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
     </>
   );
 }
