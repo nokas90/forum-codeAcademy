c539e48743e21bf42f688ed5b1e842bd36ced2c2
nokas90 <nokas90@gmail.com>
2023-12-08 20:11:25 +0200

    added textarea

--- src/components/UI/FormitInput.jsx
+++ src/components/UI/FormitInput.jsx
@@ -1,23 +1,37 @@
-const FormikInput = ({ type, name, formik, placeholder }) => {
+const FormikInput = ({ type, name, formik, placeholder, rows, columns }) => {
+  const isTextArea = type === 'textarea';
+
   return (
     <div>
       <label htmlFor={name}>{name.charAt(0).toUpperCase() + name.slice(1)}:</label>
-      <input
-        type={type}
-        name={name} id={name}
-        value={formik.values[name]} // values['hihi'] === values.hihi 
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        placeholder={placeholder?placeholder:''}
-      />
-      {
-        formik.touched[name] && formik.errors[name] &&
-        <p
-          style={{ color:"red" }}
-        >{formik.errors[name]}</p>
-      }
+      {isTextArea ? (
+        <textarea
+          name={name}
+          id={name}
+          value={formik.values[name]}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder={placeholder ? placeholder : ''}
+          rows={rows}
+          columns={columns}
+          style={{ resize: 'none' }} 
+        />
+      ) : (
+        <input
+          type={type}
+          name={name}
+          id={name}
+          value={formik.values[name]}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder={placeholder ? placeholder : ''}
+        />
+      )}
+      {formik.touched[name] && formik.errors[name] && (
+        <p style={{ color: "red" }}>{formik.errors[name]}</p>
+      )}
     </div>
   );
-}
- 
+};
+
 export default FormikInput;
