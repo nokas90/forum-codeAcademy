e93e9e433050011bd65881458d52bc0f498426dc
nokas90 <nokas90@gmail.com>
2023-12-08 20:08:01 +0200

    changing routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AllQuestions from "./components/pages/AllQuestions";
 import AddQuestion from "./components/pages/AddQuestion";
 import AddAnswer from "./components/pages/AddAnswer";
 import EditQuestion from "./components/pages/EditQuestion";
+import EditAnswer from "./components/pages/EditAnswer";
 
 function App() {
   return (
@@ -18,13 +19,13 @@ function App() {
         <Route index element={<Main />} />
         <Route path="/questions">
           <Route path="allQuestions" element={<AllQuestions />} />
+          <Route path="newQuestion" element={<AddQuestion />} />
           <Route path=":id">
             <Route index element={<OneQuestion />} />
             <Route path="addAnswer" element={<AddAnswer />} />
+            <Route path="edit" element={<EditQuestion />} />
+            <Route path="edit/:id" element={<EditAnswer />} />
           </Route>
-          <Route path="newQuestion" element={<AddQuestion />} />
-          <Route path="edit/:id" element={<EditQuestion />}/>
-
         </Route>
         <Route path="/user">
           <Route path="login" element={<Login />} />
