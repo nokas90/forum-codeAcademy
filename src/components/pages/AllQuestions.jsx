7681c5bed9c371e23f736c057958135fa2db438f
nokas90 <nokas90@gmail.com>
2023-12-11 11:42:26 +0200

    Stylling All question component

--- src/components/pages/AllQuestions.jsx
+++ src/components/pages/AllQuestions.jsx
@@ -1,15 +1,31 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ForumQuestionsContext from "../../contexts/ForumQuestionsContext";
+import ForumAnswersContext from "../../contexts/ForumAnswersContext";
 import UsersContext from "../../contexts/UserContext";
 import { styled } from "styled-components";
 import QuestionCard from "../UI/QuestionCard";
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import Typewriter from "../UI/TypeWriter";
+
 
 const TopWrapper = styled.div`
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #311d3f;
+  /* border: solid #dfdfdf 1px; */
+  padding: 10px;
+`;
+
+const StyledHeader = styled.h1`
+  text-align: center;
+  color: #420b3b;
+
+  font-family: "Times New Roman", Times, serif;
+  font-size: 3rem;
+  margin: 15px 0px;
+`;
 
 const StyledQuestions = styled.div`
   display: flex;
@@ -17,17 +33,57 @@ const StyledQuestions = styled.div`
   gap: 10px;
   justify-content: center;
   align-items: center;
+  margin-top: 20px;
+  color: #dfdfdf;
+
+  .questionSummary {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    gap: 3px;
+
+    > div {
+      display: flex;
+      gap: 5px;
+    }
+  }
+  .questionsDate {
+    /* border: yellowgreen 1px solid; */
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    gap: 5px;
+    font-size: 0.8rem;
+  }
+  .questionContent {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 80%;
+
+    > h3 {
+      margin-bottom: 10px;
+      text-align: center;
+    }
+    > p {
+      font-size: 0.9rem;
+      color: #dfdfdf;
+    }
+  }
 `;
 const FiltersContainer = styled.div`
   display: flex;
-  /* flex-direction: column; */
   gap: 10px;
   justify-content: center;
   align-items: center;
+  > h3 {
+    color: #dfdfdf;
+  }
   > label {
     padding-left: 3px;
     font-size: 0.8rem;
-    color: #f00000;
+    color: #dfdfdf;
   }
 `;
 
@@ -35,9 +91,17 @@ const StyledCheckbox = styled.input.attrs({ type: "checkbox" })`
   margin-right: 5px;
 `;
 
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+`;
+
 const AllQuestions = () => {
   const { loggedInUser } = useContext(UsersContext);
-  const { questions } = useContext(ForumQuestionsContext);
+  const { questions, setQuestions, QuestionsActionTypes } = useContext(ForumQuestionsContext);
+  const { answers } = useContext(ForumAnswersContext);
 
   // State
   const [showAnswered, setShowAnswered] = useState(true);
@@ -55,6 +119,17 @@ const AllQuestions = () => {
     );
   };
 
+  useEffect(() => {
+    fetch(`http://localhost:8080/questions`)
+      .then((res) => res.json())
+      .then((data) =>
+        setQuestions({
+          type: QuestionsActionTypes.get_all,
+          data: data,
+        })
+      );
+  }, []);
+
   // Sorting Functions
   const compareDates = (a, b) => {
     const dateA = new Date(a.createdDate);
@@ -69,33 +144,36 @@ const AllQuestions = () => {
   };
 
   // Filtered, sorted, and sorted by criteria questions
+
   const filteredAndSortedQuestions = showAnswered
     ? questions
-    : questions.filter((question) => question.answers.length === 0);
+    : questions.filter(
+        (question) =>
+          !answers.some((answer) => answer.questionId === question.id)
+      );
 
   const sortedQuestions =
     sortCriteria === "date"
       ? filteredAndSortedQuestions.sort(compareDates)
       : filteredAndSortedQuestions.sort(compareVotes);
 
+  useEffect(() => {
+    // code
+  }, []);
+
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>All Questions</h1>
+      <StyledHeader><Typewriter text={'All Questions'} speed={50}/></StyledHeader>
       <TopWrapper>
-        {loggedInUser && (
-          <Link to="/questions/newQuestion">
-            <button>Ask Question</button>
-          </Link>
-        )}
         <FiltersContainer>
           <h3>Filters</h3>
           <label>
             <StyledCheckbox
               type="checkbox"
-              checked={showAnswered}
+              checked={!showAnswered}
               onChange={() => setShowAnswered(!showAnswered)}
             />
-            Show Answered Questions
+            Hide Answered
           </label>
           <label>
             <StyledCheckbox
@@ -103,7 +181,7 @@ const AllQuestions = () => {
               checked={sortOrder === "asc"}
               onChange={toggleSortOrder}
             />
-            Sort Ascending by Date
+            Oldest First
           </label>
           <label>
             <StyledCheckbox
@@ -115,6 +193,14 @@ const AllQuestions = () => {
           </label>
         </FiltersContainer>
       </TopWrapper>
+      {loggedInUser && (
+        <ButtonWrapper>
+          <Link to="/questions/newQuestion">
+            
+            <PostAddIcon fontSize="large"/>
+          </Link>
+        </ButtonWrapper>
+      )}
 
       <StyledQuestions>
         {sortedQuestions.map((question) => (
