e410e67a74f596d93891c14e7b74d3af549b839b
nokas90 <nokas90@gmail.com>
2023-12-11 11:47:06 +0200

    cleaning code index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { UsersProvider } from "./contexts/UserContext";
-import ForumQuestionsContext, {
-  ForumQuestionsProvider,
-} from "./contexts/ForumQuestionsContext";
+import { ForumQuestionsProvider } from "./contexts/ForumQuestionsContext";
 import { ForumAnswersProvider } from "./contexts/ForumAnswersContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
